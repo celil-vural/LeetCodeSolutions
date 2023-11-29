0ab1455e13e397e757112c8d22f7b1fe24ea64d4
celil-vural <60570862+celil-vural@users.noreply.github.com>
2023-11-29 23:26:21 +0300

    Time: 31 ms (87.20%), Space: 29.5 MB (69.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0) return false;
+        int original = x;
+        int reversed = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}
