582368478c1595fde85c68d91637475f50a697e8
celil-vural <celilvural.work@gmail.com>
2026-09-11 13:45:39 +0300

    Time: 0 ms (100%), Space: 4.9 MB (52.91%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.go
@@ -0,0 +1,21 @@
+func combinationSum(candidates []int, target int) [][]int {
+	var result [][]int
+	var current []int
+	backtrack(candidates, target, 0, current, &result)
+	return result
+}
+
+func backtrack(candidates []int, target int, start int, current []int, result *[][]int) {
+	if target == 0 {
+		*result = append(*result, append([]int(nil), current...))
+		return
+	}
+
+	for i := start; i < len(candidates); i++ {
+		if candidates[i] <= target {
+			current = append(current, candidates[i])
+			backtrack(candidates, target-candidates[i], i, current, result)
+			current = current[:len(current)-1]
+		}
+	}
+}
