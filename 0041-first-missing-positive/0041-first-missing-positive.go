ebe0cc0ad5504c44549fefa30b58d234d6a36730
celil-vural <celilvural.work@gmail.com>
2026-09-11 14:24:06 +0300

    Time: 0 ms (100%), Space: 10.9 MB (25%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.go
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -1,6 +1,6 @@
 func firstMissingPositive(nums []int) int {
 	n := len(nums)
-	for i := 0; i < n; i++ {
+	for i := range nums {
 		for nums[i] >= 1 &&
 			nums[i] <= n &&
 			nums[nums[i]-1] != nums[i] {
@@ -8,7 +8,7 @@ func firstMissingPositive(nums []int) int {
 			nums[i], nums[j] = nums[j], nums[i]
 		}
 	}
-	for i := 0; i < n; i++ {
+	for i := range nums {
 		if nums[i] != i+1 {
 			return i + 1
 		}
